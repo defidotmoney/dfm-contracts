0b300302b0617dd65438e6c7fbaae7831b9f58a5
Cowboy <consol3cowboy@proton.me>
2024-03-12 05:55:52 +0000

    docs: interfaces with natspec

---
+++ interfaces/IControllerHooks.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+
+/**
+    @dev Controller hooks are set per-function. Controller Hooks contracts must
+         only implement the interfaces of the hooks which will be active.
+
+         Each function returns `int256 debtAdjustment`.
+         * A positive value creates more debt, charging a fee to the account.
+         * A negative value creates less debt, giving a rebate to the account.
+ */
+interface IControllerHooks {
+
+    /**
+        @dev Called when creating a new loan
+        @return debtAdjustment Adjustment amount to the new debt created
+                * The minted amount is exactly `debtAmount`
+                * The adjustment is applied to the amount of debt owed to the market
+     */
+    function on_create_loan(
+        address account,
+        address market,
+        uint256 collAmount,
+        uint256 debtAmount
+    ) external returns (int256 debtAdjustment);
+
+    /**
+        @dev Called when adjusting an existing loan
+        @return debtAdjustment Debt adjustment amount
+                * the change to debt is exactly `debtChange`. A positive value means minting, negative burning.
+                * The adjustment is applied to the amount of debt owed to the market.
+     */
+    function on_adjust_loan(
+        address account,
+        address market,
+        int256 collChange,
+        int256 debtChange
+    ) external returns (int256 debtAdjustment);
+
+    /**
+        @dev Called when closing an existing loan
+        @return debtAdjustment Adjustment amount on debt owed
+                * the amount of debt reduced is exactly `accountDebt`
+                * the adjustment is applied to the amount of tokens burned from the caller's address
+     */
+    function on_close_loan(
+        address account,
+        address market,
+        uint256 accountDebt
+    ) external returns (int256 debtAdjustment);
+
+    /**
+        @dev Called when an existing loan is liquidated
+        @return debtAdjustment Adjustment amount on liquidated owed.
+                * the amount of debt reduced is exactly `debtLiquidated`
+                * the adjustment is applied to the amount of tokens burned from the caller's address
+     */
+    function on_liquidation(
+        address caller,
+        address market,
+        address target,
+        uint256 debtLiquidated
+    ) external returns (int256 debtAdjustment);
+}
