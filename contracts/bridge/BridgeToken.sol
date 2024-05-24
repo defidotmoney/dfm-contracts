6f740e85c242c35461dd86ec76887d70742d7002
Cowboy <cowboy@defi.money>
2024-05-24 23:14:38 +0000

    feat: simple OFT quote/send functions

--- contracts/bridge/BridgeToken.sol
+++ contracts/bridge/BridgeToken.sol
@@ -2,7 +2,9 @@
 
 pragma solidity 0.8.25;
 
+import { MessagingFee } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/interfaces/IOFT.sol";
 import "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
+import { OFTMsgCodec } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/libs/OFTMsgCodec.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "../interfaces/IProtocolCore.sol";
@@ -35,6 +37,43 @@ contract BridgeToken is OFT, ERC20FlashMint {
     ) OFT(_name, _symbol, _lzEndpoint, msg.sender) {
         CORE_OWNER = _core;
         _setDefaultOptions(_defaultOptions);
+    }
+
+    /**
+        @notice Simplified version of `OFT.quoteSend`
+        @dev Uses default messaging options, assumes destination chain is EVM compatible
+             and that bridge fee is paid in the chain's native token.
+        @param _eid Endpoint ID of the remote chain to bridge tokens to.
+        @param _target Target address to receive tokens on destination chain.
+        @param _amount Amount of tokens to bridge.
+        @return nativeFee Required fee amount in chain's native gas token.
+     */
+    function quoteSimple(uint32 _eid, address _target, uint256 _amount) external view returns (uint256) {
+        (, uint256 amountReceivedLD) = _debitView(_amount, _amount, _eid);
+
+        (bytes memory message, ) = OFTMsgCodec.encode(_addressToBytes32(_target), _toSD(amountReceivedLD), bytes(""));
+        return _quote(_eid, message, enforcedOptions[_eid][1], false).nativeFee;
+    }
+
+    /**
+        @notice Simplified version of `OFT.send`
+        @dev Assumes destination chain is EVMcompatible, uses default options, gives
+             `msg.value` as the bridge fee and refunds excess fees to the caller.
+        @param _eid Endpoint ID of the remote chain to bridge tokens to.
+        @param _target Target address to receive tokens on destination chain.
+        @param _amount Amount of tokens to bridge.
+        @return amountSentLD Actual amount of tokens that were bridged.
+     */
+    function sendSimple(uint32 _eid, address _target, uint256 _amount) external payable returns (uint256) {
+        (uint256 amountSentLD, uint256 amountReceivedLD) = _debit(msg.sender, _amount, _amount, _eid);
+
+        (bytes memory message, ) = OFTMsgCodec.encode(_addressToBytes32(_target), _toSD(amountReceivedLD), bytes(""));
+        bytes memory options = enforcedOptions[_eid][1];
+
+        bytes32 guid = _lzSend(_eid, message, options, MessagingFee(msg.value, 0), msg.sender).guid;
+
+        emit OFTSent(guid, _eid, msg.sender, amountSentLD, amountReceivedLD);
+        return amountSentLD;
     }
 
     function setMinter(address minter, bool isApproved) external onlyOwner returns (bool) {
@@ -75,10 +114,14 @@ contract BridgeToken is OFT, ERC20FlashMint {
         @dev https://docs.layerzero.network/v2/developers/evm/oft/quickstart#message-execution-options
              Use `0x0003010011010000000000000000000000000000ea60` to send
              60,000 gas for each token bridge message.
-
      */
     function setDefaultOptions(bytes memory options) external onlyOwner {
         _setDefaultOptions(options);
+    }
+
+    /** @dev Convert address to layerzero-formatted bytes32 peer */
+    function _addressToBytes32(address account) internal pure returns (bytes32) {
+        return bytes32(uint256(uint160(account)));
     }
 
     function _setPeer(uint32 _eid, bytes32 _peer) internal override {
