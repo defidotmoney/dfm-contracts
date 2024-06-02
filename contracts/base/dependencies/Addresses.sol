85045271d174873edd6f5af82add0df4b276d772
Cowboy <cowboy@defi.money>
2024-06-02 15:27:18 +0000

    feat: add guardian role within protocol core

--- contracts/base/dependencies/Addresses.sol
+++ contracts/base/dependencies/Addresses.sol
@@ -5,4 +5,5 @@ pragma solidity ^0.8.0;
 library Addresses {
     bytes32 internal constant BRIDGE_RELAY = bytes32("BRIDGE_RELAY");
     bytes32 internal constant FEE_RECEIVER = bytes32("FEE_RECEIVER");
+    bytes32 internal constant GUARDIAN = bytes32("GUARDIAN");
 }
