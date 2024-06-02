c38309c90f1c6ecf914f2c7f258702df866a5ccc
Cowboy <cowboy@defi.money>
2024-06-02 18:30:06 +0000

    test: aggregate chained oracle tests

--- tests/brownie/oracles/conftest.py
+++ tests/brownie/oracles/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 
+from brownie import ZERO_ADDRESS
+
 
 @pytest.fixture(scope="module")
 def uptime_cl(ChainlinkAggregatorMock, deployer):
@@ -12,3 +14,28 @@ def uptime_cl(ChainlinkAggregatorMock, deployer):
 @pytest.fixture(scope="module")
 def uptime_oracle(Layer2UptimeOracle, uptime_cl, deployer):
     return Layer2UptimeOracle.deploy(uptime_cl, {"from": deployer})
+
+
+@pytest.fixture(scope="module")
+def uniswap(MockUniOracleReader, deployer):
+    return MockUniOracleReader.deploy({"from": deployer})
+
+
+@pytest.fixture(scope="module")
+def curve(TricryptoMock, deployer):
+    return TricryptoMock.deploy([ZERO_ADDRESS] * 3, {"from": deployer})
+
+
+@pytest.fixture(scope="module")
+def curve2(TricryptoMock, deployer):
+    return TricryptoMock.deploy([ZERO_ADDRESS] * 3, {"from": deployer})
+
+
+@pytest.fixture(scope="module")
+def curve3(TricryptoMock, deployer):
+    return TricryptoMock.deploy([ZERO_ADDRESS] * 3, {"from": deployer})
+
+
+@pytest.fixture(scope="module")
+def chained_oracle(AggregateChainedOracle, core, deployer):
+    return AggregateChainedOracle.deploy(core, ZERO_ADDRESS, {"from": deployer})
