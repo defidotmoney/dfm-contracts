5133f44725a5f206e9a6e323fa242c40f25eac70
consol3cowboy <cowboy@defi.money>
2024-07-10 13:31:53 +0000

    feat: implement regulator logic

--- contracts/interfaces/IStakerRewardRegulator.sol
+++ contracts/interfaces/IStakerRewardRegulator.sol
@@ -8,7 +8,7 @@ interface IStakerRewardRegulator {
         @dev * Called once per day from `StableStaker`.
              * The return value must be less than or equal to the initial amount.
              * `amount - stakerAmount` is the amount sent onward to `GovStaker`.
-        @param amount The available reward amount for the new peiord.
+        @param amount The available reward amount for the new period.
         @return stakerAmount Reward amount for the stable staker.
      */
     function getStakerRewardAmount(uint256 amount) external returns (uint256 stakerAmount);
