149ea4a6f9beba7bf49df9b74f807bdc67d1f4b1
consol3cowboy <cowboy@defi.money>
2024-07-23 11:19:07 +0000

    refactor: shared votium/votemarket logic in base contract

--- contracts/fees/ReceiverVoteMarket.sol
+++ contracts/fees/ReceiverVoteMarket.sol
@@ -2,70 +2,38 @@
 
 pragma solidity 0.8.25;
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import { IVoteMarket } from "../interfaces/external/IVoteMarket.sol";
 import { IFeeReceiver } from "../interfaces/IFeeReceiver.sol";
-import { TokenRecovery } from "./dependencies/TokenRecovery.sol";
+import { GaugeAllocReceiverBase } from "./dependencies/GaugeAllocReceiverBase.sol";
 
-contract ReceiverVoteMarket is TokenRecovery, IFeeReceiver {
-    using EnumerableSet for EnumerableSet.AddressSet;
-
-    uint256 constant MIN_AMOUNT = 100 * 1e18;
+/**
+    @notice StakeDao VoteMarket Receiver
+    @author defidotmoney
+    @dev Receives fees from `PrimaryFeeAggregator` and deposits into VoteMarket
+         https://github.com/stake-dao/x-chain-vm/blob/23fe83a/src/Platform.sol
+ */
+contract VoteMarketFeeReceiver is IFeeReceiver, GaugeAllocReceiverBase {
     uint8 constant BOUNTY_PERIODS = 4;
     uint256 constant MAX_PRICE_PER_VOTE = type(uint256).max;
 
     address public immutable feeAggregator;
-    IERC20 public immutable stableCoin;
     IVoteMarket public immutable voteMarket;
-
-    EnumerableSet.AddressSet private __gauges;
-    EnumerableSet.AddressSet private __blacklist;
-
-    uint256 public totalAllocationPoints;
-
-    mapping(address gauge => uint256 allocPoints) public gaugeAllocationPoints;
-    mapping(address gauge => uint256 id) private bountyIds;
-
-    struct GaugeAlloc {
-        address gauge;
-        uint256 points;
-    }
 
     event BountyCreated(address indexed gauge, uint256 bountyId);
     event BountyRewardAdded(address indexed gauge, uint256 indexed bountyId, uint256 rewardAmount);
 
-    event BlacklistSet(address account, bool isBlacklisted);
-    event GaugeAllocationSet(address gauge, uint256 allocPoints);
+    mapping(address gauge => uint256 id) private bountyIds;
 
     constructor(
         address core,
-        IERC20 _stable,
+        address _stable,
         address _feeAggregator,
-        IVoteMarket _voteMarket,
+        address _voteMarket,
         GaugeAlloc[] memory _gauges,
-        address[] memory _blacklist
-    ) TokenRecovery(core) {
+        address[] memory _excluded
+    ) GaugeAllocReceiverBase(core, _stable, _voteMarket, _gauges, _excluded) {
         feeAggregator = _feeAggregator;
-        stableCoin = _stable;
-        voteMarket = _voteMarket;
-
-        _setGauges(_gauges);
-        _setBlacklist(_blacklist, true);
-
-        _stable.approve(address(_voteMarket), type(uint256).max);
-    }
-
-    function getGaugeCount() external view returns (uint256) {
-        return __gauges.length();
-    }
-
-    function isGaugeAdded(address gauge) external view returns (bool) {
-        return __gauges.contains(gauge);
-    }
-
-    function getGaugeList() external view returns (address[] memory) {
-        return __gauges.values();
+        voteMarket = IVoteMarket(_voteMarket);
     }
 
     function quoteNotifyNewFees(uint256) external view returns (uint256) {
@@ -73,16 +41,17 @@ contract ReceiverVoteMarket is TokenRecovery, IFeeReceiver {
     }
 
     function notifyNewFees(uint256) external payable {
-        require(msg.sender == feeAggregator);
+        require(msg.sender == feeAggregator, "DFM: Only feeAggregator");
 
-        uint256 count = __gauges.length();
-        if (count == 0) return;
+        address[] memory gauges = getGaugeList();
+        uint256 length = gauges.length;
+        if (length == 0) return;
 
         uint256 total = stableCoin.balanceOf(address(this));
         uint256 totalAlloc = totalAllocationPoints;
 
-        for (uint256 i = 0; i < count; i++) {
-            address gauge = __gauges.at(i);
+        for (uint256 i = 0; i < length; i++) {
+            address gauge = gauges[i];
             uint256 amount = (total * gaugeAllocationPoints[gauge]) / totalAlloc;
             if (amount < MIN_AMOUNT) continue;
 
@@ -101,7 +70,7 @@ contract ReceiverVoteMarket is TokenRecovery, IFeeReceiver {
                     BOUNTY_PERIODS,
                     MAX_PRICE_PER_VOTE,
                     amount,
-                    __blacklist.values(),
+                    getExclusionList(),
                     true
                 );
                 bountyIds[gauge] = bountyId + 1;
@@ -118,45 +87,6 @@ contract ReceiverVoteMarket is TokenRecovery, IFeeReceiver {
         if (msg.value != 0) {
             (bool success, ) = msg.sender.call{ value: msg.value }("");
             require(success, "DFM: Gas refund transfer failed");
-        }
-    }
-
-    function setGauges(GaugeAlloc[] calldata gauges) external onlyOwner {
-        _setGauges(gauges);
-    }
-
-    function setBlacklist(address[] calldata accounts, bool isBlacklisted) external onlyOwner {
-        _setBlacklist(accounts, isBlacklisted);
-    }
-
-    function _setGauges(GaugeAlloc[] memory gauges) internal {
-        uint256 totalAlloc = totalAllocationPoints;
-        uint256 length = gauges.length;
-
-        for (uint i = 0; i < length; i++) {
-            address gauge = gauges[i].gauge;
-            uint256 points = gauges[i].points;
-            if (points > 0) {
-                if (!__gauges.add(gauge)) {
-                    totalAlloc -= gaugeAllocationPoints[gauge];
-                }
-                totalAlloc += points;
-            } else {
-                require(__gauges.remove(gauge), "DFM: cannot remove unset gauge");
-                totalAlloc -= gaugeAllocationPoints[gauge];
-            }
-            emit GaugeAllocationSet(gauge, points);
-            gaugeAllocationPoints[gauge] = points;
-        }
-        totalAllocationPoints = totalAlloc;
-    }
-
-    function _setBlacklist(address[] memory accounts, bool isBlacklisted) internal {
-        uint256 length = accounts.length;
-        for (uint i = 0; i < length; i++) {
-            if (isBlacklisted) require(__blacklist.add(accounts[i]), "DFM: Account already added");
-            else require(__blacklist.remove(accounts[i]), "DFM: Account not on list");
-            emit BlacklistSet(accounts[i], isBlacklisted);
         }
     }
 }
--- contracts/interfaces/external/IVotium.sol
+++ contracts/interfaces/external/IVotium.sol
@@ -5,11 +5,12 @@ pragma solidity ^0.8.0;
 interface IVotium {
     function activeRound() external view returns (uint256);
 
-    function depositSplitGauges(
+    /** @dev deposit same token to multiple gauges with different amounts in a single round */
+    function depositUnevenSplitGauges(
         address _token,
-        uint256 _amount,
         uint256 _round,
-        address[] calldata _gauges,
+        address[] memory _gauges,
+        uint256[] calldata _amounts,
         uint256 _maxPerVote,
         address[] calldata _excluded
     ) external;
